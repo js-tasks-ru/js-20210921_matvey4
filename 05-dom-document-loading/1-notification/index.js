ed0d1034408b4551231b778c8ad53068104b3b60
mat179 <mat179@users.noreply.github.com>
2021-10-06 02:07:14 +0300

    Chapter #5 task. NotificationMessage implemented.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+  static activeNotification;
+
+  constructor(msg, {duration = 2000,
+    type = 'success'} = {}) {
+    this.msg = msg;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  get template() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                        <div class="notification-header">${this.type}</div>
+                        <div class="notification-body">
+                                ${this.msg}
+                        </div>
+                </div>
+            </div>`;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.destroy();
+    }
+
+    target.append(this.element);
+    NotificationMessage.activeNotification = this;
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    // NOTE: удаляем обработчики событий, если они есть
+  }
 
 }
