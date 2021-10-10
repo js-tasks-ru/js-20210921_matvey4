2b8db30397c393c1adaec76d956524d63afd26cc
mat179 <mat179@users.noreply.github.com>
2021-10-10 17:52:43 +0300

    Chapter #6 tasks. SortableTable v2 and tooltip implemented.

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,69 @@
 class Tooltip {
+  static activeTooltip;
+  element;
 
+  onPointerOut = () => {
+    document.removeEventListener('pointermove', this.onPointerMove);
+    this.remove();
+  }
+
+
+  onPointerOver = event => {
+    const htmlEl = event.target.closest('[data-tooltip]');
+    if (htmlEl) {
+      this.render(htmlEl.dataset.tooltip);
+      document.addEventListener('pointermove', this.onPointerMove);
+    }
+  }
+
+  onPointerMove = event => {
+    this.move(event);
+  }
+
+  constructor() {
+    if (Tooltip.activeTooltip) {
+      return Tooltip.activeTooltip;
+    }
+
+    Tooltip.activeTooltip = this;
+  }
+
+  render(text) {
+    this.element = document.createElement('div');
+    this.element.classList.add('tooltip');
+    this.element.innerHTML = text;
+
+    document.body.append(this.element);
+  }
+
+  initialize() {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  move(event) {
+    const left = event.clientX + 15;
+    const top = event.clientY + 15;
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+  }
+  remove () {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointerout', this.onPointerOut);
+    this.remove();
+  }
 }
 
 export default Tooltip;
